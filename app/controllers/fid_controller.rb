dd057fd066f50becd89cf19f57d0760c15ed4e56
sebamoralesasd <sebasuni96@gmail.com>
2025-09-22 12:30:47 -0300

    comment

--- app/controllers/fid_controller.rb
+++ app/controllers/fid_controller.rb
@@ -2,6 +2,7 @@ require_relative "../services/fid_services/feed_sources"
 
 class FidController < ApplicationController
   def index
+    # TODO: mover.
     urls = FidServices::FeedSources.urls
     @feed = []
     providers = urls.flat_map do |url|
